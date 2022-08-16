903e981ff4784eb6d7ec00b6c2fc1ccc7e08760a
Tosin Owoeye <101506432+TFrankie55@users.noreply.github.com>
2022-08-16 22:13:39 +0100

    Update sort.h

--- sort.h
+++ sort.h
@@ -4,14 +4,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* comparison direction macros for bitonic sort */
+/* Comparison direction macros for bitonic sort */
 #define UP 0
 #define DOWN 1
 
 /**
- * enum bool - enumeration of boolean values.
- * @false: Equals 0
- * @true: Equals 1
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
  */
 typedef enum bool
 {
@@ -21,6 +21,7 @@ typedef enum bool
 
 /**
  * struct listint_s - Doubly linked list node
+ *
  * @n: Integer stored in the node
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
@@ -34,9 +35,9 @@ typedef struct listint_s
 
 /* Printing helper functions */
 void print_array(const int *array, size_t size);
-void print_list(const  listint_t *list);
+void print_list(const listint_t *list);
 
-/* Sorting algorithms */
+/* Sorting algoritms */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
@@ -49,6 +50,5 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-void sort_deck(deck_node_t **deck);
 
 #endif /* SORT_H */
